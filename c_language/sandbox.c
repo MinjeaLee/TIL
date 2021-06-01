c26758ba348ab41b99ca3571b776bfbd7cb6f769
Minjea Lee <gabe0529@naver.com>
2021-06-01 22:58:02 +0900

    C 2021.06.01

--- c_language/sandbox.c
+++ c_language/sandbox.c
@@ -2,9 +2,29 @@
 
 
 
+// int main(){
+//     double a;
+//     a = (10 + 20 + 230 + 40 + 54) / 5.0;
+//     printf("%lf", a);
+//     return 0;
+// /
+
+int a, b;
+
+int swap(int *a, int *b){
+    int tmp;
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
 int main(){
-    double a;
-    a = (10 + 20 + 230 + 40 + 54) / 5.0;
-    printf("%lf", a);
-    return 0;
+    int a = 1, b = 2;
+    int *pa, *pb;
+    pa = &a;
+    pb = &b;
+
+    swap(pa, pb);
+    printf("%d %d", *pa, *pb);
+
 }
