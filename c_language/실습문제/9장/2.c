39c4f765a9ea94f1084addfd854acc7d0f064a6f
Minjea Lee <gabe0529@naver.com>
2021-06-02 15:44:35 +0900

    C _pointer 2021.06.02

--- c_language/실습문제/9장/2.c
+++ c_language/실습문제/9장/2.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
 int main(){
+    char string[20], a;
+    int *p_ch;
+
+    for(p_ch = string; p_ch < string + 20; p_ch++){
+        scanf("%c", p_ch);
+        if(*p_ch == '\n'){
+            break;
+        }
+    }
     
+    
+    printf("%s", *p_ch);
+
+
 
     return 0;
 }
