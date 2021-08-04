6c0920a16f0f62325fb61156ee7f38da87703bcb
Minjea Lee <gabe0529@naver.com>
2021-08-04 16:50:14 +0900

    vs_code dir change 2021.08.04

--- c_language/study/20210626.c
+++ c_language/study/20210626.c
@@ -1,24 +1,55 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int len(char x[]){
-    int result = 0;
-    while (x[result] != 0)
-    {
-        result++;
-    }
-    return result;
+// int len(char x[]){
+//     int result = 0;
+//     while (x[result] != 0)
+//     {
+//         result++;
+//     }
+//     return result;
+// }
+
+// int main(){
+//     char a[] = "Don't worry, Be happy!";
+//     char b[] = "걱정마, 행복할 거야.";
+//     int a_len, b_len;
+
+//     // a_len = len(a);
+//     // b_len = len(b);
+//     a_len = strlen(a);
+//     b_len = strlen(b);
+
+//     printf("a_len = %d\nb_len = %d\n", a_len, b_len);
+
+//     strcpy(a, b);
+//     strcat(a, b);
+
+//     printf("%s\n%s", a, b);
+
+//     return 0;
+// }
+
+
+void test(){
+
+
 }
+
+struct sdasd
+{
+    int p1, p2;
+    char a[13];
+};
+
+
 
 int main(){
-    char a[] = "Don't worry, Be happy!";
-    char b[] = "걱정마, 행복할 거야.";
-    int a_len, b_len;
+    struct sdasd k;
+    k.p1 = 123;
+    printf("%d", k.p1);
 
-    a_len = len(a);
-    b_len = len(b);
 
-    printf("a_len = %d\nb_len = %d\n", a_len, b_len);
-    printf("%s\n%s", a, b);
 
     return 0;
-}
