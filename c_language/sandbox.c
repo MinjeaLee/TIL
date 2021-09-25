ec3346593eb645c22ead575bdb5e2d38e34804d3
Minjea Lee <gabe0529@naver.com>
2021-09-25 20:01:53 +0900

    C sutdy 2021.09.25

--- c_language/sandbox.c
+++ c_language/sandbox.c
@@ -1,23 +1,100 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void swap(char *x, char *y){
-    char tmp;
+void merge(int low, int mid, int high);
+void mergeSort(int low, int high);
+void printArr(int a[], int n);
+void copyArray(int start, int end);
 
-    tmp = *x;
-    *x = *y;
-    *y = tmp;
+#define number 100000
+static int mergeArr1[number];
+static int mergeArr2[number];
+
+void mergeSort(int low, int high) {
+    int mid;
+    if(low < high) {
+        mid = (low + high)/2;
+        mergeSort(low, mid);
+        mergeSort(mid+1, high);
+        merge(low, mid, high);
+	}
 }
 
-int main(){
-    char str_2[] = "123";
-    int len_2 = 3;
+void merge(int low, int mid, int high) {
 
-    for(int i = 0; i < len_2 / 2; i++){
-        swap(str_2 + i, str_2 + len_2 - 1 - i);
+    int i = low;
+    int j = mid+1;
+    int k = low;
+
+    while (i<=mid && j<=high) {
+        if(mergeArr2[i] < mergeArr2[j]) {
+           mergeArr1[k++] = mergeArr2[i++];
+        } else if(mergeArr2[i] >= mergeArr2[j]) {
+           mergeArr1[k++] = mergeArr2[j++];
+        }
     }
 
-    
-    printf("%s", str_2);
+ 
+
+    // 남은 영역 조사후 mergedArr으로 복사
+
+    if(i>=mid) {
+        while(j<=high) {
+           mergeArr1[k++] = mergeArr2[j++];
+        }
+    }
+
+    if(j>=high) {
+        while(i<=mid) {
+           mergeArr1[k++] = mergeArr2[i++];
+        }
+    }
+    copyArray(low, high);
+}
+
+ 
+
+// 배열 출력 함수 
+
+void printArr(int a[], int n) {
+     int i;
+     for (i=0; i<n; i++) {
+        printf("%d ", a[i]);
+     }
+     printf("\n");
+}
+
+ 
+
+void copyArray(int start, int end) {
+    int i;
+    for (i=start; i<=end; i++) {
+        mergeArr2[i] = mergeArr1[i];
+    }
+}
+
+ 
+
+int main(int argc, char *argv[]) {
+
+	int i,n;
+	 
+    printf("몇개의 숫자로 정렬하시겠습니까?\n");
+    scanf("%d",&n);
+
+    for(i = 0 ; i < n ; i++)
+	mergeArr2[i]=rand()%1000;
+  
+    printf("정렬전 배열 : ");
+    printArr(mergeArr2,n);
+
+    mergeSort(0, n-1);
+
+	printf("정렬후 배열 : ");
+    printArr(mergeArr2,n); 
+
+    system("PAUSE");
 
     return 0;
+
 }
--- c_language/고급_C/과제/9장/1-2.c
+++ c_language/고급_C/과제/9장/1-2.c
@@ -2,89 +2,90 @@
 #include <limits.h>
 
 int *max(int *x);
-
 int *min(int *x);
+int *low_address(int *a, int *b);
+int dif_address(int *a, int *b);
 
-// 함수 원형 선언 마지막으로
-int *low_address(int *a, int *b){
-    int *add;
-    if(a > b){
-        add = b;
-    }
-    else{
-        add = a;
-    }
-    return add;
-}
-int dif_address(int *a, int *b){
-    int *high, *low;
-    if(a > b){
-        high = a;
-        low = b;
-    }
-    else{
-        low = a;
-        high = b;
-    }
-    if(high - low <= 1){
-        return 0;
-    }
-    else{
-        return high - low;
-    }
-}
 
 int main(){
-    int n, arr[100];
-    int *p_n = &n, *p_arr;
-    int dif;
+    int n, arr[100];   // 사용자에게 받은 변수, 배열 선언
+    int *p_n = &n, *p_arr; // 포인터 변수 선언
+    int dif;            // 주소 차이를 저장할 변수 선언
 
 
-    scanf("%d", p_n);
+    scanf("%d", p_n);   // 정수 입력
 
-    for(int i = 0; i < *p_n; i++){
-        for(p_arr = arr; p_arr < arr + 100; p_arr++){
-            scanf("%d", p_arr);
-            if(*p_arr == 0){
+    for(int i = 0; i < *p_n; i++){  // 입력받은 정수 만큼 반복
+        for(p_arr = arr; p_arr < arr + 100; p_arr++){   // 배열에 정수 입력
+            scanf("%d", p_arr);                 // 정수 입력
+            if(*p_arr == 0){                    // 0일때까지 입력받기
                 break;
             }
         }
-        if(dif_address(max(arr), min(arr))){
-            dif = dif_address(max(arr), min(arr));
-            for(p_arr = low_address(max(arr), min(arr)) + 1; p_arr < low_address(max(arr), min(arr)) + dif; p_arr++){
-                printf("%d ", *p_arr);
+        if(dif_address(max(arr), min(arr))){        // 최댓값과 최솟값의 주소 차이가 양수 일경우
+            dif = dif_address(max(arr), min(arr));  // 그 차이를 변수에 저장
+            for(p_arr = low_address(max(arr), min(arr)) + 1; p_arr < low_address(max(arr), min(arr)) + dif; p_arr++){ // 낮은 주소 부터 높은 주소 까지 반복
+                printf("%d ", *p_arr);  // 사이에 있는 값 출력
             }
-            printf("\n");
+            printf("\n");       // 다음 출력을 위해 줄바꿈
         }
         else{
-            printf("none\n");
+            printf("none\n");   // 차이가 없다면 none 출력
         }
     }
 
     return 0;
 }
 
+
 int *max(int *x){
-    int max = INT_MIN;
-    int *p_max = &max;
-    for(int *p = x; *p != 0; p++){
-        if(*p > max){
-            max = *p;
-            p_max = p;
+    int max = INT_MIN;  // 반환할 변수, 정수 최솟값으로 초기화
+    int *p_max = &max;  // 포인터 변수 선언 초기화
+    for(int *p = x; *p != 0; p++){ // 원소가 0이 아닐 때까지 반복
+        if(*p > max){ // 지금까지의 최댓값보다 클 경우
+            max = *p;   // 해당 값을 최댓값으로
+            p_max = p;  // 주소 또한 초기화
         }
     }
-    return p_max;
+    return p_max;   // 최댓값 주소 반환
 }
 
-int *min(int *x){
-    int min = INT_MAX;
-    int *p_min = &min;
-    for(int *p = x; *p != 0; p++){
-        if(*p < min){
-            min = *p;
-            p_min = p;
+int *min(int *x){ 
+    int min = INT_MAX;  // 반환할 변수, 정수 최댓값으로 초기화
+    int *p_min = &min;  // 포인터 변수 선언, 초기화
+    for(int *p = x; *p != 0; p++){  // 원소가 0이 아닐때까지 반복
+        if(*p < min){   // 지금까지의 최솟값보다 작을 경우
+            min = *p;   // 해당값 최솟값
+            p_min = p;  // 주소 또한 초기화
         }
     }
-    return p_min;
+    return p_min;   // 최솟값 주소 반환
 }
 
+int *low_address(int *a, int *b){   // 최솟값 최대값의 주소 중 더 낮은 주소 반환
+    int *add;   // 반환할 포인터 변수 선언
+    if(a > b){  // a의 주소가 더 높으면 b의 주소를 저장
+        add = b;
+    }
+    else{       // b의 주소가 더 높으면 a의 주소 저장
+        add = a;
+    }
+    return add; // 주소 반환
+}
+int dif_address(int *a, int *b){    // 주소의 차이를 반환할 함수
+    int *high, *low;    // 높은 주소, 낮의 주소를 저장할 포인터 변수 선언
+    if(a > b){      // 높은 주소, 낮은 주소를 각각 저장
+        high = a;
+        low = b;
+    }
+    else{           // 높은 주소, 낮은 주소를 각각 저장
+        low = a;
+        high = b;
+    }
+    if(high - low <= 1){    // 만약 주소의 차이가 1 이하인 경우 연달아 있는 경우와 차이가 없는 경우 0 반환
+        return 0;
+    }
+    else{                   // 그외의 경우 그 차이를 반환
+        return high - low;
+    }
+}
