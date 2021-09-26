ad5cb730d0e87733878d56baf9ca18506077fc1c
Minjea Lee <gabe0529@naver.com>
2021-09-26 19:12:21 +0900

    c study 2021.09.26

--- c_language/we.c
+++ c_language/we.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
 int main(){
-    for(int i = 0; i < 1; i++){
-        printf("asd\n");
+    char str[20];
+    char str_2[5];
+
+    for(int i = 0; i < 20 - 5 + 1; i++){
+        str[i + 0] == str_2[i % 5 + 0];
+        str[i + 1] == str_2[i % 5 + 1];
+        str[i + 2] == str_2[i % 5 + 2];
+        str[i + 3] == str_2[i % 5 + 3];
+        str[i + 4] == str_2[i % 5 + 4];
+
     }
+    
     return 0;
 }
