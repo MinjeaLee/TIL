b82f223a6be3931828e5228bccca1663f82f2298
Minjea Lee <gabe0529@naver.com>
2021-09-28 05:20:22 +0900

    c study 2021.09.28

--- c_language/s!_c_/과제/1차시/1.c
+++ c_language/s!_c_/과제/1차시/1.c
@@ -1,7 +1,57 @@
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+void append(char *dst, char c) {
+    char *p = dst;
+    while (*p != '\0') p++; // 문자열 끝 탐색
+    *p = c;
+    *(p+1) = '\0'; 
+}
+char* strrev_1(char* str){
+    int startIdx, endIdx;
+    if(!str || !*str)    return (char *)-1;
+    startIdx = 0;
+    endIdx = strlen(str)-1;
+    while(startIdx<endIdx) {
+        char tmp = str[startIdx];
+        str[startIdx] = str[endIdx];
+        str[endIdx] = tmp;
+        startIdx ++;
+        endIdx --;
+    }
+    return str;
+} 
 
 int main(){
-    
+    int n, num, extra;
+    char print_num[2048], tmp;
+
+
+    scanf("%d", &n);
+    num = pow(n, 2);
+    extra = num / 9;
+    num += extra;
+    print_num[0] = '\0';
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n - i - 1; j++){
+            printf(" ");
+        }
+        for(int j = 0; j < 2 * i + 1; j++){
+            if(num % 10 == 0){
+                num--;
+            }
+            tmp = (num % 10) + '0';
+            append(print_num, tmp);
+            num--;
+        }
+        printf("%s", strrev_1(print_num));
+        if(i != n -1){
+            printf("\n");
+        }
+        print_num[0] = '\0';
+    }
 
     return 0;
 }
--- c_language/we.c
+++ c_language/we.c
@@ -1,36 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    char str[20];
-    char str_2[5];
+    printf("%d", 7 / 2);
 
-    for(int i = 0; i < 20 - 5 + 1; i++){
-        str[i + 0] == str_2[i % 5 + 0];
-        str[i + 1] == str_2[i % 5 + 1];
-        str[i + 2] == str_2[i % 5 + 2];
-        str[i + 3] == str_2[i % 5 + 3];
-        str[i + 4] == str_2[i % 5 + 4];
-
-    }
-    
     return 0;
 }
-
-
-
-
-1234567890 i < 5
-23          j < 2
-
-i = 0
-
-    j = 0, x = 0 
-    j = 1, x = 1  
-
-i = 1
-    j = 0, 2 
-    j = 1, 3
-
-i = 2
-    j = 0, 4
-    
