eeb9041aeb53e912adc40a195f0c6202e9dd6986
Minjea Lee <gabe0529@naver.com>
2021-10-11 11:08:56 +0900

    C study 2021.10.11

--- c_language/Advanced_C/assignment/chapter_11/1-1.c
+++ c_language/Advanced_C/assignment/chapter_11/1-1.c
@@ -3,25 +3,25 @@
 
 typedef struct sutduent{
 
-    char name[10], grade;
-    int id;
+    char name[10], grade;       // 학생이름, 등급
+    int id;                 // 학번
 
 }stu;
 
 int main(){
 
-    stu chart[5];
-    char check_name[10];
+    stu chart[5];   // 5명 구조체 배열 선언
+    char check_name[10];            // 확인할 학생 이름 받을 문자열 선언
 
-    for(int i = 0; i < 5; i++){
-        scanf("%s %d %c", &chart[i].name, &chart[i].id, &chart[i].grade);
-        getchar();
+    for(int i = 0; i < 5; i++){ // 5명 반복
+        scanf("%s %d %c", &chart[i].name, &chart[i].id, &chart[i].grade);       // 이름 학번 성적 입력
+        getchar();      // \n getchar
     }
-    scanf("%s", check_name);
+    scanf("%s", check_name);        // 확인할 학생 이름 
 
-    for(int i = 0; i < 5; i++){
-        if(strcmp(check_name, chart[i].name) == 0){
-            printf(" %d %c", chart[i].id, chart[i].grade);
+    for(int i = 0; i < 5; i++){     // 배열확인
+        if(strcmp(check_name, chart[i].name) == 0){     // 입력받은 이름과 같은 것이 있다면
+            printf(" %d %c", chart[i].id, chart[i].grade);      // 학생 정보 출력
         }
     }
 
