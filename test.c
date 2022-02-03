526865c43877f7279a7ea6327f4f8c06a49a21a0
Minjea Lee <gabe0529@naver.com>
2022-02-03 22:20:38 +0900

    copilot 2022.02.03

---
+++ test.c
@@ -0,0 +1,21 @@
+// Create a program that only receives a string that does not contain spaces from the user and then prints only the lowercase letter.
+
+// Input Example 1: HelloWorld
+// Output Example 1: elloorld
+// Input Example 2: AbCdEEff
+// Output Example 2: bdff
+
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+    int i = 0;
+    
+
+    return 0;
+}
